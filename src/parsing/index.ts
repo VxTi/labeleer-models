89fc7e13af461096a964bb10af1c3b5b425a915a
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2025-12-12 15:35:57 +0100

    Further renamed files for consistency, added barrel exports

---
+++ src/parsing/index.ts
@@ -0,0 +1,6 @@
+export * from './android-strings-parser';
+export * from './apple-strings-parser';
+export * from './json-parser';
+export * from './po-parser';
+export * from './qt-linquist-parser';
+export * from './yaml-parser';
---
+++ src/serialization/index.ts
@@ -0,0 +1,7 @@
+export * from './apple-strings-serializer';
+export * from './android-strings-serializer';
+export * from './json-serializer';
+export * from './po-serialization';
+export * from './qt-linquist-serializer';
+export * from './xliff-serializer';
+export * from './yaml-serializer';
